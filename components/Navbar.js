d4a8d69ed4051c488b076f535bb1eb4fbf86594f
AmiteshSingh45 <amiteshsinghrajput00831@gmail.com>
2025-02-08 19:57:23 +0530

    register and login page database se connect ho chuka hai

--- components/Navbar.js
+++ components/Navbar.js
@@ -90,7 +90,7 @@ const Navbar = () => {
           </button>
         </Link>
 
-        <Link href="/login">
+        <Link href="/Login">
           <button className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 font-medium rounded-lg text-sm px-5 py-2.5">
             Login
           </button>
