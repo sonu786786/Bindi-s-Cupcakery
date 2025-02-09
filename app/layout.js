081b5ec1b7039b49a68740d89d65923e786753f5
sonu <psonu786786@gmail.com>
2025-02-09 15:32:53 +0530

    about_us page

--- app/layout.js
+++ app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
       >
         <Navbar/>
         <div className="min-h-[82vh]">
-          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-          <Toaster position="bottom-center" />
+          {/* <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+          <Toaster position="bottom-center" /> */}
         {children}
         </div>
         <Footer/>
